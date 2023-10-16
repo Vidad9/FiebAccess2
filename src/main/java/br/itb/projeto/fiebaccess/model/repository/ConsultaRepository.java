00a093b324d7fa9866432225a5b3c645a8633d2b
Vidad9 <100845894+Vidad9@users.noreply.github.com>
2023-10-16 14:21:20 -0300

    Delete src directory

--- src/main/java/br/itb/projeto/fiebaccess/model/repository/ConsultaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.itb.projeto.fiebaccess.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.itb.projeto.fiebaccess.model.entity.Consulta;
-
-public interface ConsultaRepository extends JpaRepository<Consulta, Long>{
-
-}
--- src/main/java/br/itb/projeto/fiebaccess/model/repository/ProfissionalRepository.java
+++
@@ -1,9 +0,0 @@
-package br.itb.projeto.fiebaccess.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.itb.projeto.fiebaccess.model.entity.Profissional;
-
-public interface ProfissionalRepository extends JpaRepository<Profissional, Long> {
-
-}
