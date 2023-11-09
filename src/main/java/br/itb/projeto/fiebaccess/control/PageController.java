13f3c2bf147e4ab0ac36c077da0fb7829e3de735
GilWell <105554047+DevGILWELL@users.noreply.github.com>
2023-11-09 11:22:25 -0300

    Add files via upload

---
+++ src/main/java/br/itb/projeto/fiebaccess/control/PageController.java
@@ -0,0 +1,18 @@
+package br.itb.projeto.fiebaccess.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+	
+@GetMapping("/")
+public String getIndex() {
+	return "land1";
+}
+	
+	
+
+}
